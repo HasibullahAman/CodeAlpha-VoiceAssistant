eb7c40b914bac3fcbd07b6f4be6bad2ad37ada6a
Hasibullah Aman <hasib.aman7@gmail.com>
2024-07-13 22:58:53 +0430

    this is just for tackling text and readin

--- CodeAlpha_VoiceAssistantProject/VoiceAssistant.py
+++ CodeAlpha_VoiceAssistantProject/VoiceAssistant.py
@@ -1,1 +1,15 @@
 import os #provides a collection of functions for interacting with the operating system
+import time
+import playsound
+import speech_recognition as sr 
+from gtts import gTTS
+
+
+def speak(text): # this is just for tackling text and reading
+    tts = gTTS(text=text, lang='en', slow=False)
+    filename = 'temp.mp3'
+    tts.save(filename)
+    playsound.playsound(filename)
+    os.remove(filename)
+    
+speak("Hello, I am Hasibullah Aman and I want to marry with you!")
