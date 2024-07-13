051249237c3de329a9cbebfde869e21b56fff566
Hasibullah Aman <hasib.aman7@gmail.com>
2024-07-14 00:03:17 +0430

    work on get sound as input

--- CodeAlpha_VoiceAssistantProject/VoiceAssistant.py
+++ CodeAlpha_VoiceAssistantProject/VoiceAssistant.py
@@ -5,11 +5,32 @@ import speech_recognition as sr
 from gtts import gTTS
 
 
-def speak(text): # this is just for tackling text and reading
+def speak(text): # this is just for tacking text and reading
     tts = gTTS(text=text, lang='en', slow=False)
     filename = 'temp.mp3'
     tts.save(filename)
     playsound.playsound(filename)
     os.remove(filename)
     
-speak("Hello, I am Hasibullah Aman and I want to marry with you!")
+# speak("Please say everything you want!")
+
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+        try:
+            said = r.recognize_google(audio)
+            print("You said: ", said)
+        except Exception as e:
+            print("Exception: ", str(e))
+    return said
+
+
+text = get_audio()
+
+if "hello" in text:
+    speak("Hello!, How are you doing, it's timee to speak!")
+if "my name is" in text:
+    speak("My name is HUA robat!, happy to know you know what can I do for you!?")
