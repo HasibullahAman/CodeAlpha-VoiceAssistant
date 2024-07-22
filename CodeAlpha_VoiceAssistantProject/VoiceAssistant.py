a641b30083faf979fd1b82a51a2d489810df7fa8
Hasibullah Aman <hasib.aman7@gmail.com>
2024-07-22 22:18:21 +0430

    try to use pyttsx3 instead of gtts or google voice API

--- CodeAlpha_VoiceAssistantProject/VoiceAssistant.py
+++ CodeAlpha_VoiceAssistantProject/VoiceAssistant.py
@@ -12,9 +12,8 @@ from google.auth.transport.requests import Request
 
 import os  # provides a collection of functions for interacting with the operating system
 import time
-import playsound
 import speech_recognition as sr
-from gtts import gTTS
+import pyttsx3
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
@@ -37,12 +36,13 @@ DAY_EXTENTIONS = ["rd", "th", "st", "nd"]
 
 
 def speak(text):  # this is just for tacking text and reading
-    tts = gTTS(text=text, lang="en", slow=False)
-    filename = "temp.mp3"
-    tts.save(filename)
-    playsound.playsound(filename)
-    os.close(filename)  # we close mp3 file before the anohter task
-    os.remove(filename)
+    
+    # tts = gTTS(text=text, lang="en", slow=False)
+    # filename = "temp.mp3"
+    # tts.save(filename)
+    # playsound.playsound(filename)
+    # os.close(filename)  # we close mp3 file before the anohter task
+    # os.remove(filename)
 
 
 # speak("Please say everything you want!")
