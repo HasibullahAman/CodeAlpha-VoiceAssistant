a89209def88cdcb7bbba3eb9190864f0b84d35ae
Hasibullah Aman <hasib.aman7@gmail.com>
2024-08-03 22:53:02 +0430

    try to work with wikipedia

---
+++ CodeAlpha_VoiceAssistantProject/helper.py
@@ -0,0 +1,54 @@
+# import when I get the Google Calendar API
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# inner import
+from startApp import startApp
+from open_app import webAssist
+
+
+
+
+# If modifying these scopes, delete the file token.pickle.
+# SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+import os  # provides a collection of functions for interacting with the operating system
+import time
+import speech_recognition as sr
+import pyttsx3
+import pytz # This library is essential for handling time zones accurately and reliably in Python.
+import subprocess
+
+
+
+import subprocess
+# import wolframalpha
+import pyttsx3
+import tkinter
+# import json
+# import random
+# import operator
+import speech_recognition as sr
+import datetime
+import wikipedia
+# import webbrowser
+import os
+# import winshell
+# import pyjokes
+# import feedparser
+# import smtplib
+# import ctypes
+import time
+# import requests
+# import shutil
+# from twilio.rest import Client
+# from clint.textui import progress
+# from ecapture import ecapture as ec
+# from bs4 import BeautifulSoup
+# import win32com.client as wincl
+# from urllib.request import urlopen
