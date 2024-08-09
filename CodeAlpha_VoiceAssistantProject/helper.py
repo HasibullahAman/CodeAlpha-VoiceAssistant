bba99d635082e6f8b877940bf3b8ae9f9a610633
Hasibullah Aman <hasib.aman7@gmail.com>
2024-08-09 13:55:46 +0430

    remove some comment code from helper

--- CodeAlpha_VoiceAssistantProject/helper.py
+++ CodeAlpha_VoiceAssistantProject/helper.py
@@ -1,14 +1,3 @@
-# # import when I get the Google Calendar API
-# from __future__ import print_function
-# import datetime
-# import pickle
-# import os.path
-# from googleapiclient.discovery import build
-# from google_auth_oauthlib.flow import InstalledAppFlow
-# from google.auth.transport.requests import Request
-# # If modifying these scopes, delete the file token.pickle.
-# # SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
-
 import os  # provides a collection of functions for interacting with the operating system
 import time
 import speech_recognition as sr
